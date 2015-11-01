ec57f416ad3a9905462958795a59fa8d8732502c
Thomas Reto Strub <ths@strubt.ch>
2015-11-01 11:13:48 +0100

    initial commit

---
+++ src/ch/bfh/progressor/executor/Executor.java
@@ -0,0 +1,19 @@
+package ch.bfh.progressor.executor;
+
+/**
+ * Main class.
+ * Starts the executor
+ *
+ * @author strut1, touwm1 &amp; weidj1
+ */
+public final class Executor {
+
+	private Executor() {
+		//static class
+	}
+
+	public static void main(String... args) {
+
+		System.err.println("Not yet implemented.");
+	}
+}
---
+++ src/ch/bfh/progressor/executor/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Root package for the 'Progressor' component 'Executor'.
+ * Contains all classes necessary for the code executor to run.
+ *
+ * @author strut1, touwm1 &amp; weidj1
+ */
+package ch.bfh.progressor.executor;
