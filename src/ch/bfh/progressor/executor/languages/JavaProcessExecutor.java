9d4d2a4d00511c6dec105f0d03ef61c9cba28828
Thomas Reto Strub <thomas.strub@students.bfh.ch>
2015-11-26 09:28:14 +0100

    bugfix

--- src/ch/bfh/progressor/executor/languages/JavaProcessExecutor.java
+++ src/ch/bfh/progressor/executor/languages/JavaProcessExecutor.java
@@ -92,7 +92,7 @@ public class JavaProcessExecutor implements CodeExecutor {
 					outStm.useDelimiter(CodeExecutor.END_OF_LINE + CodeExecutor.END_OF_LINE);
 					while (outStm.hasNext()) {
 						String res = outStm.next();
-						ret.add(new Result().setSuccess(res.startsWith("OK")).setResult(res.substring(2)));
+						ret.add(new Result().setSuccess(res.startsWith("OK")).setResult(res.substring(3)));
 					}
 				}
 
